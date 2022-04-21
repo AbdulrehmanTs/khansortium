11a64dbff37b1848e980365a5763063ab3af0fe0
AbdulrehmanTs <rohi644@gmail.com>
2022-04-21 23:54:50 +0500

    pic change

--- src/components/landing/FbPost.jsx
+++ src/components/landing/FbPost.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import { AiFillLike } from 'react-icons/ai';
 import { FaCommentAlt, FaFacebook } from 'react-icons/fa';
 import { RiShareForwardFill } from 'react-icons/ri';
-import profilePic from '../../assets/pageprofile.png'
+import profilePic from '../../assets/pageprofile.jpeg'
 
 
 const FbPost = ({ width, height, src }) => {
     const { full_picture, created_time, permalink_url, from, actions, picture } = src
     const date = new Date(created_time).toDateString()
-    console.log(created_time);
     return (
         // <iframe src={src} width={width} height={height} style={{border:"none", overflow:"hidden"}} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         <a href={permalink_url} className='fb_post' style={{ width, height, backgroundImage: `url(${full_picture})`, backgroundSize: "cover", backgroundRepeat: 'no-repeat', backgroundPosition: 'center', }}>
             <div className='post_headings' >
-                <img src={profilePic} className='profile_pic' alt='profile' />   {/* <img src="" alt="" /> */}
+                <img src={profilePic} style={{ objectFit: 'cover' }} className='profile_pic' alt='profile' />   {/* <img src="" alt="" /> */}
                 <div className='page_name'>
                     <h2>{from.name}</h2>
                     <small>{date}</small>
